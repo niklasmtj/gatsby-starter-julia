fd3affd1bc4df700048c6ba568782d022222aa75
Robert Skarżycki <3417357+robert-skarzycki@users.noreply.github.com>
2020-01-12 18:42:02 +0100

    Get rid of gatsby-plugin-web-font-loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,14 +65,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-web-font-loader`,
-      options: {
-        google: {
-          families: [`Nunito:400,700`],
-        },
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
